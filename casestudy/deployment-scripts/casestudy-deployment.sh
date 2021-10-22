958ca82621631f7a9dd3a54f4ef7279e00d057fa
bbuyuka <burhan.buyukarslan@adesso.com.tr>
2021-10-22 20:56:46 +0300

    İkinci yükleme

--- casestudy/deployment-scripts/casestudy-deployment.sh
+++ casestudy/deployment-scripts/casestudy-deployment.sh
@@ -1,11 +1,16 @@
 cd ../ || exit
 
 helm repo add elastic https://Helm.elastic.co
-helm install --name kibana elastic/kibana
+#helm repo add fluent https://fluent.github.io/helm-charts
+
+helm install kibana elastic/kibana
+#helm install --name fluent-bit fluent/fluent-bit
+
 helm repo update
 
 
-helm upgrade --install elasticsearch elastic/elasticsearch -f ./values/elk/casestudy-elastic-values.yaml  -n casestudy --create-namespace
+helm upgrade --install elasticsearch elastic/elasticsearch -f ./values/elk/casestudy-elastic-values.yaml -n casestudy --create-namespace
 
-helm upgrade --install kibana elastic/kibana -f ./values/elk/casestudy-kibana-values.yaml  -n casestudy --create-namespace
+helm upgrade --install kibana elastic/kibana -f ./values/elk/casestudy-kibana-values.yaml -n casestudy --create-namespace
 
+#helm upgrade --install fluent-bit fluent/fluent-bit -f values/efk/casestudy-fluentbit-values.yaml -n casestudy --create-namespace
