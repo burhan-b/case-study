ebee50a2a762ec7c043f144bff83718cea37d4ff
bbuyuka <burhan.buyukarslan@adesso.com.tr>
2021-10-23 18:26:50 +0300

    Son hali

--- casestudy/deployment-scripts/casestudy-deployment.sh
+++ casestudy/deployment-scripts/casestudy-deployment.sh
@@ -1,16 +1,10 @@
 cd ../ || exit
 
-helm repo add elastic https://Helm.elastic.co
-#helm repo add fluent https://fluent.github.io/helm-charts
-
-helm install kibana elastic/kibana
-#helm install --name fluent-bit fluent/fluent-bit
-
+helm repo add elastic https://helm.elastic.co
 helm repo update
 
+helm upgrade --install elasticsearch elastic/elasticsearch -f ./values/efk/cs-elasticsearch-dev-values.yaml -n casestudy --create-namespace
 
-helm upgrade --install elasticsearch elastic/elasticsearch -f ./values/elk/casestudy-elastic-values.yaml -n casestudy --create-namespace
+helm upgrade --install kibana elastic/kibana -f ./values/efk/cs-kibana-dev-values.yaml -n casestudy --create-namespace
 
-helm upgrade --install kibana elastic/kibana -f ./values/elk/casestudy-kibana-values.yaml -n casestudy --create-namespace
-
-#helm upgrade --install fluent-bit fluent/fluent-bit -f values/efk/casestudy-fluentbit-values.yaml -n casestudy --create-namespace
+# add "192.168.99.104 dev.casestudy.com" to etc/hosts file
