de884c46fa2ba7928d99d51b8ea26cd32375b6a0
bbuyuka <burhan.buyukarslan@adesso.com.tr>
2021-10-24 21:48:25 +0300

    düzenleme ve not ekleme

--- casestudy/deployment-scripts/casestudy-deployment.sh
+++ casestudy/deployment-scripts/casestudy-deployment.sh
@@ -1,14 +1,16 @@
 cd ../ || exit
 
+#gerekli repo ekleniyor
 helm repo add elastic https://helm.elastic.co
 helm repo update
 
+#Elasticsearch deploy ediliyor
 helm upgrade --install elasticsearch elastic/elasticsearch -f ./values/efk/cs-elasticsearch-dev-values.yaml -n casestudy --create-namespace
 
+#Kibana deploy ediliyor
 helm upgrade --install kibana elastic/kibana -f ./values/efk/cs-kibana-dev-values.yaml -n casestudy --create-namespace
 
-kubectl apply -f ./charts/configmaps/fluentbit-configmap.yaml
-
+#Nginx ve Fluentbit deploy ediliyor
 helm upgrade --install nginx ./charts -n casestudy --create-namespace
 
-# add "192.168.99.104 dev.casestudy.com" to etc/hosts file
+# "192.168.99.104 dev.casestudy.com" satırı etc/hosts dosyasına eklenmeli
