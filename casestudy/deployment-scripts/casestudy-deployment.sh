17300aba0ed9562045758b8dce3c60f95fe98892
bbuyuka <burhan.buyukarslan@adesso.com.tr>
2021-10-26 09:30:10 +0300

    ingress controller

--- casestudy/deployment-scripts/casestudy-deployment.sh
+++ casestudy/deployment-scripts/casestudy-deployment.sh
@@ -1,8 +1,15 @@
 cd ../ || exit
 
 #Gerekli repo ekleniyor
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add elastic https://helm.elastic.co
 helm repo update
+
+#ingress controller namespace
+NAMESPACE=ingress-basic
+
+#ingress controller deploy ediliyor
+helm install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace $NAMESPACE
 
 #Elasticsearch deploy ediliyor
 helm upgrade --install elasticsearch elastic/elasticsearch -f ./values/efk/cs-elasticsearch-dev-values.yaml -n casestudy --create-namespace
